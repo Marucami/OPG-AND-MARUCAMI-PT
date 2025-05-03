45dd714795ea19a72d17d605ce4c7e3e256ff7d1
FreshinS <freshinsboost@gmail.com>
2025-05-03 20:53:24 +0300

    splitted js and html file, and created electron app

---
+++ main.js
@@ -0,0 +1,16 @@
+import { app, BrowserWindow } from "electron"
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1600,
+    height: 720
+  })
+
+  win.removeMenu()
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
